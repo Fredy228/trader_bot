0e63f07f7f665e735d544a9996733554e59a6f55
Fredy228 <smagrovich58@gmail.com>
2025-04-13 20:20:40 +0300

    Add mode work

--- bot/mode/strategy_1/test_strategy_1.py
+++ bot/mode/strategy_1/test_strategy_1.py
@@ -1,54 +1,15 @@
 from pprint import pprint
 import plotly.graph_objects as go
 
-from operations.candles import get_candles
+from operations.candles import get_candles_from_date
 from operations.extremes import find_extremes
 from operations.trend import plot_trend
 
-from config import SYMBOL, BARS, TIMEFRAME, DEBUG_MODE
+from config import SYMBOL, BARS, TIMEFRAME, FROM_DATE, DEBUG_MODE
 
 
 def test_strategy_1():
     print("\nИдет получение данных...\n")
-    df = get_candles(SYMBOL, TIMEFRAME, BARS)
+    df = get_candles_from_date(SYMBOL, TIMEFRAME, BARS, FROM_DATE)
     if DEBUG_MODE == 1:
         print("candles:\n", df)
-
-    print("\nПоиск экстремумов...\n")
-    swings = find_extremes(df=df)
-    if DEBUG_MODE == 1:
-        pprint(swings)
-
-    print("\nПостроение тренда...\n")
-    hours_line, values_line = plot_trend(
-        swings=swings, candle={"high": df["high"][0], "low": df["low"][0]}
-    )
-
-    fig = go.Figure(
-        data=[
-            go.Candlestick(
-                x=df["time"],
-                open=df["open"],
-                high=df["high"],
-                low=df["low"],
-                close=df["close"],
-            )
-        ]
-    )
-
-    fig.add_trace(
-        go.Scatter(
-            x=hours_line,
-            y=values_line,
-            mode="lines+markers",
-            name="Трендовая линия",
-            line=dict(color="red", width=2, dash="solid"),
-        )
-    )
-
-    fig.update_layout(
-        title=f"Анализ тренда: {SYMBOL}",
-        xaxis_title="Время",
-        yaxis_title="Цена",
-        xaxis_rangeslider_visible=True,
-    )
--- bot/operations/candles.py
+++ bot/operations/candles.py
@@ -1,9 +1,23 @@
 import MetaTrader5 as mt5
 import pandas as pd
+from datetime import datetime
 
 
 def get_candles(currency="EURUSD", timeframe=mt5.TIMEFRAME_H1, bars=50):
     rates = mt5.copy_rates_from_pos(currency, timeframe, 0, bars)
+    df = pd.DataFrame(rates)
+    df["time"] = pd.to_datetime(df["time"], unit="s")
+    df["open"] = df["open"].astype(str)
+    df["high"] = df["high"].astype(str)
+    df["low"] = df["low"].astype(str)
+    df["close"] = df["close"].astype(str)
+    return df[["time", "open", "high", "low", "close"]]
+
+
+def get_candles_from_date(
+    currency="EURUSD", timeframe=mt5.TIMEFRAME_H1, from_date=None
+):
+    rates = mt5.copy_rates_range(currency, timeframe, from_date, datetime.now())
     df = pd.DataFrame(rates)
     df["time"] = pd.to_datetime(df["time"], unit="s")
     df["open"] = df["open"].astype(str)
